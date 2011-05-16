4f3b1d90f3f672af5e6b76bc2dc4edc1b59b721d
Simon Sapin <simon.sapin@exyr.org>
2011-05-16 10:32:58 +0200

    Prefix test module names with 'test_', even if it’s redundant.

--- weasy/tests/__init__.py
+++ weasy/tests/__init__.py
@@ -24,5 +24,5 @@ def resource_filename(basename):
     return os.path.join(os.path.dirname(__file__), 'resources', basename)
 
 all = Tests('.'.join((__name__, mod, 'suite'))
-            for mod in ('css',
-                        'css_properties'))
+            for mod in ('test_css',
+                        'test_css_properties'))
