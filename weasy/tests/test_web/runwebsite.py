5a65afb11179dd2297448666f8e6fd3de0587166
Simon Sapin <simon.sapin@exyr.org>
2011-08-05 12:09:58 +0200

    Add default input for the test web app.

--- weasy/tests/test_web/runwebsite.py
+++ weasy/tests/test_web/runwebsite.py
@@ -13,19 +13,47 @@ INPUT = os.path.join(app.root_path, 'input.html')
 PNG_OUTPUT = os.path.join(app.root_path, 'output.png')
 PDF_OUTPUT = os.path.join(app.root_path, 'output.pdf')
 
+DEFAULT_CONTENT = """
+<style>
+html {
+    background-color:gray;
+    font-family: DejaVu Sans Mono;
+    font-size:15px;
+}
+p {
+    width:480px;
+    margin:40px;
+    padding: 10px;
+    border-width:20px;
+    vertical-align:middle;
+    border-width:10px;
+    border-style:solid;
+}
+h1 {
+    text-decoration : underline;
+}
+</style>
+<h1>Avancement de weasyprint</h1>
+<p>test test </p>
+"""
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method != 'POST':
-        with open(INPUT) as fd:
-            content = fd.read()
+        if os.path.isfile(INPUT):
+            with open(INPUT) as fd:
+                content = fd.read()
+        else:
+            content = DEFAULT_CONTENT
         return render_template('index.html.jinja2', content=content, image=None)
 
     content = request.form.get("content", "").strip()
 
-    # Save the input HTML
-    with open(INPUT, 'w') as fd:
-        fd.write(content)
+    if content:
+        # Save the input HTML
+        with open(INPUT, 'w') as fd:
+            fd.write(content)
 
     pdf_document = PDFDocument.from_string(content)
     pdf_document.output = PDF_OUTPUT
