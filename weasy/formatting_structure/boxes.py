7a3088737e999ddd99bcc4cf03fa599baec23354
Simon Sapin <simon.sapin@exyr.org>
2011-08-08 15:58:38 +0200

    Remove misleading None values for Box.height and Box.width.

--- weasy/formatting_structure/boxes.py
+++ weasy/formatting_structure/boxes.py
@@ -86,9 +86,6 @@ class Box(object):
         # children. Only the root box should stay without a parent.
         self.parent = None
         self._init_style()
-        # When the size is not calculated yet, use None as default value
-        self.width = None
-        self.height = None
 
     def _init_style(self):
         # Computed values
