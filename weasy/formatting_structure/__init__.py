30b9fc74cb59744dc19ff2757deb97f16463d8d7
Guillaume Ayoub <guillaume.ayoub@kozea.fr>
2011-08-24 10:05:07 +0200

    Add a docstring in weasy/formatting_structure/__init__

--- weasy/formatting_structure/__init__.py
+++ weasy/formatting_structure/__init__.py
@@ -15,3 +15,8 @@
 #
 #  You should have received a copy of the GNU Affero General Public License
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+Formatting structures of the CSS.
+
+"""
