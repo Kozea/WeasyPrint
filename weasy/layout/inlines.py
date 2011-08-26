72b4086ed9edd1b1a5f1a87b552dd01908b4aa25
Salem Harrache <sharrache@kozea.fr>
2011-08-26 09:47:45 +0200

    catch a TypeError exception in split_inline_box to waiting to find the cause

--- weasy/layout/inlines.py
+++ weasy/layout/inlines.py
@@ -368,10 +368,17 @@ def split_inline_box(inlinebox, remaining_width):
 
     """
     assert isinstance(inlinebox, boxes.InlineBox)
-    left_spacing = (inlinebox.padding_left + inlinebox.margin_left +
-                    inlinebox.border_left_width)
-    right_spacing = (inlinebox.padding_right + inlinebox.margin_right +
-                     inlinebox.border_right_width)
+    resolve_percentages(inlinebox)
+    try:
+        left_spacing = (inlinebox.padding_left + inlinebox.margin_left +
+                        inlinebox.border_left_width)
+        right_spacing = (inlinebox.padding_right + inlinebox.margin_right +
+                         inlinebox.border_right_width)
+    except TypeError:
+        for side in ['left', 'top', 'right', 'bottom']:
+            inlinebox.reset_spacing(side)
+        left_spacing = 0
+        right_spacing = 0
     remaining_width -= left_spacing
 
     new_inlinebox = inlinebox.copy()
