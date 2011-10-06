54721e2c86367a17c0083d1b39eaab876111f001
Simon Sapin <simon.sapin@exyr.org>
2011-10-06 12:19:10 +0200

    Add config for gorun.

---
+++ gorun_settings.py
@@ -0,0 +1,9 @@
+# See https://github.com/peterbe/python-gorun
+
+# Note! Order matters which makes it possible to run particular
+# command if a file changes in a directory and another command for all
+# other files in that directory.
+
+DIRECTORIES = (
+  ('weasy', 'attest'),
+)
