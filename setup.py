d76602b0857d668e6723539e73cc1a95a807169c
Simon Sapin <simon.sapin@exyr.org>
2011-11-02 09:52:17 +0100

    Add some metadata in setup.py

--- setup.py
+++ setup.py
@@ -14,9 +14,21 @@ setup(
     name='WeasyPrint',
     version='0.2dev',  # Also change this in weasy/__init__.py
     url='http://weasyprint.org/',
-    license='GNU Affero General Public License',
+    license='GNU Affero General Public License v3',
     description='WeasyPrint converts web documents to PDF.',
     long_description=__doc__,
+    author='Simon Sapin',
+    author_email='simon.sapin@kozea.fr',
+    plateforms='Any',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Affero General Public License v3',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Text Processing :: Markup :: HTML',
+        'Topic :: Printing',
+    ],
     packages=find_packages(),
     package_data={
         'weasy.tests': ['resources/*',
