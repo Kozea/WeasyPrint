af28de57ff823e7410a627e02a8945f39abb47ac
Simon Sapin <simon.sapin@exyr.org>
2012-01-25 20:30:15 +0100

    Iterate on FireWeasy.

--- fireweasy/serialize.py
+++ fireweasy/serialize.py
@@ -1,4 +1,10 @@
+from __future__ import division
+
 import functools
+from io import BytesIO
+import cairo
+
+
 
 BOX_ATTRIBUTES = '''
     position_x position_y
@@ -16,26 +22,55 @@ def serialize(document):
     Serialize a WeasyPrint Document object to JSON-compatible data types
     for in-browser rendering.
     """
-    return {'pages': map(serialize_page_box, document.pages)}
+    return {'pages': [
+        serialize_page_box(document, page)
+        for page in document.pages]}
 
 
-def serialize_box(box, attributes=BOX_ATTRIBUTES):
+def serialize_box(document, box, attributes=BOX_ATTRIBUTES):
     rv = {name: getattr(box, name) for name in attributes}
-    rv['style'] = serialize_style(box.style)
+    rv['style'] = serialize_style(document, box.style)
 
     children = getattr(box, 'children', None)
     if children is not None:
-        rv['children'] = map(serialize_box, children)
+        rv['children'] = [serialize_box(document, child) for child in children]
 
     marker = getattr(box, 'outside_list_marker', None)
     if marker is not None:
-        rv['children'].append(serialize_box(marker))
+        rv['children'].append(serialize_box(document, marker))
 
     text = getattr(box, 'text', None)
     if text is not None:
         rv['text'] = text
 
+    replacement = getattr(box, 'replacement', None)
+    if replacement is not None:
+        width = int(box.width)
+        height = int(box.height)
+        rv['width'] = width
+        rv['height'] = height
+        rv['image'] = serialize_replacement(replacement, width, height)
+
     return rv
+
+
+def serialize_replacement(replacement, scaled_width=None, scaled_height=None):
+    pattern, width, height = replacement
+
+    if scaled_width is None:
+        scaled_width = width
+    if scaled_height is None:
+        scaled_height = height
+
+    surface = cairo.ImageSurface(
+        cairo.FORMAT_ARGB32, scaled_width, scaled_height)
+    context = cairo.Context(surface)
+    context.scale(scaled_width / width, scaled_height / height)
+    context.set_source(pattern)
+    context.paint()
+    file_like = BytesIO()
+    surface.write_to_png(file_like)
+    return file_like.getvalue().encode('base64').replace('\n', '')
 
 
 @apply
@@ -43,12 +78,21 @@ def serialize_page_box():
     return functools.partial(serialize_box, attributes=PAGE_BOX_ATTRIBUTES)
 
 
-def serialize_style(style):
+def serialize_style(document, style):
     style = style.as_dict()
     for key, value in style.iteritems():
         if getattr(value, 'type', None) == 'COLOR_VALUE':
             style[key] = value.cssText
+    bg_image = style['background_image']
+    if bg_image == 'None':
+        bg_image = None
+    else:
+        bg_image = document.get_image_from_uri(style['background_image'])
+        if bg_image is not None:
+            bg_image = serialize_replacement(bg_image)
+    style['background_image'] = bg_image
     style['background_position'] = [
-        getattr(v, 'cssText', v) for v in style['background_position']]
-    style['text_decoration'] = list(style['text_decoration'])
+        getattr(v, 'cssText', v)
+        for v in style['background_position']]
+    style['text_decoration'] = ' '.join(style['text_decoration']) or 'none'
     return style
--- fireweasy/static/draw.coffee
+++ fireweasy/static/draw.coffee
@@ -1,27 +1,48 @@
 window.FireWeasy = (($) ->
-    draw_box = (box, page) ->
-        div = $('<div></div>').appendTo(page).css(
+    draw_box = (box, x=0, y=0) ->
+        # box.position_x and box.position_y are from the page corner,
+        # but left and top in CSS with 'position: absolute' refer
+        # to the padding area of the closest positionned ancesteor.
+        # So we track that with x and y.
+        style = box.style
+        css =
             position: 'absolute'
-            top: box.position_y
-            left: box.position_x
+            left: box.position_x - x
+            top: box.position_y - y
             width: box.width
             height: box.height
-        )
+        for name in ['font-size', 'font-family', 'font-weight',
+                     'text-decoration', 'background-color', 'background-repeat']
+            css[name] = style[name.replace('-', '_')]
+        if style.background_image
+            css['background-image'] = 'url(data:image/png;base64,' +
+                style.background_image + ')'
+        for side in ['top', 'bottom', 'left', 'right']
+            css['margin-' + side] = style['margin_' + side]
+            css['padding-' + side] = style['padding_' + side]
+            for prop in ['width', 'style', 'color']
+                css['border-' + side + '-' + prop] =
+                    style['border_' + side + '_' + prop]
+        if box.image
+            div = $('<img>').attr('src', 'data:image/png;base64,' + box.image)
+        else
+            div = $('<div></div>')
+        div.css(css)
         if box.text
             div.text(box.text)
+        x = box.position_x + box.margin_left + box.border_left_width
+        y = box.position_y + box.margin_top + box.border_top_width
         for child in box.children or []
-            draw_box(child, page)
+            div.append(draw_box(child, x, y))
+        div
 
-    draw_page = (page) ->
-        section = $('<section></section>').css(
+    draw_page = (page) -> $('<section></section>').append(draw_box(page)).css(
             position: 'relative'
             width: page.outer_width + 'px'
             height: page.outer_height + 'px'
             margin: '20px'
-            'box-shadow': '3px 3px 3px 3px #888')
-        draw_box(page, section)
-        section
+            'box-shadow': '3px 3px 3px 3px #666')
 
     draw: (document, container) ->
-        container.append.apply(container, $.map(document.pages, draw_page))
+        container.append($.map(document.pages, draw_page)...)
 )(jQuery)
