280dcbd12116c7611eb3ca45184f587626a54fe2
Simon Sapin <simon.sapin@exyr.org>
2012-05-21 14:55:40 +0200

    Test bookmark labels and positions.

--- weasyprint/tests/test_pdf.py
+++ weasyprint/tests/test_pdf.py
@@ -45,11 +45,17 @@ def get_metadata(html):
     return pdf.gather_metadata(document)
 
 
-def get_bookmarks(html):
+def get_bookmarks(html, structure_only=False):
     (root, bookmarks), links, destinations = get_metadata(html)
     for bookmark in bookmarks:
-        bookmark.pop('destination')
-        bookmark.pop('label')
+        if structure_only:
+            bookmark.pop('destination')
+            bookmark.pop('label')
+        else:
+            # Eliminate errors of floating point arithmetic
+            # (eg. 499.99999999999994 instead of 500)
+            p, x, y = bookmark['destination']
+            bookmark['destination'] = p, round(x, 6), round(y, 6)
     return root, bookmarks
 
 
@@ -63,13 +69,14 @@ def test_bookmarks():
     root, bookmarks = get_bookmarks('''
         <style>
             @page { -weasy-size: 1000pt; margin: 0 }
-            * { height: 100pt }
+            * { height: 90pt; margin: 0 0 10pt 0; page-break-inside: auto }
         </style>
         <h1>Title 1</h1>
         <h1>Title 2</h1>
-        <h2>Title 3</h2>
+        <div style="margin-left: 50pt"><h2>Title 3</h2></div>
         <h2>Title 4</h2>
-        <h3>Title 5<div style="page-break-before: always"></div></h3>
+        <h3>Title 5<span style="display: block;
+                                page-break-before: always"></span></h3>
         <h2>Title 6</h2>
         <h1>Title 7</h1>
         <h2>Title 8</h2>
@@ -79,17 +86,28 @@ def test_bookmarks():
     ''')
     assert root == dict(Count=11, First=1, Last=10)
     assert bookmarks == [
-        dict(Count=0, First=None, Last=None, Next=2, Parent=0, Prev=None),
-        dict(Count=4, First=3, Last=6, Next=7, Parent=0, Prev=1),
-        dict(Count=0, First=None, Last=None, Next=4, Parent=2, Prev=None),
-        dict(Count=1, First=5, Last=5, Next=6, Parent=2, Prev=3),
-        dict(Count=0, First=None, Last=None, Next=None, Parent=4, Prev=None),
-        dict(Count=0, First=None, Last=None, Next=None, Parent=2, Prev=4),
-        dict(Count=2, First=8, Last=8, Next=10, Parent=0, Prev=2),
-        dict(Count=1, First=9, Last=9, Next=None, Parent=7, Prev=None),
-        dict(Count=0, First=None, Last=None, Next=None, Parent=8, Prev=None),
-        dict(Count=1, First=11, Last=11, Next=None, Parent=0, Prev=7),
-        dict(Count=0, First=None, Last=None, Next=None, Parent=10, Prev=None)]
+        dict(Count=0, First=None, Last=None, Next=2, Parent=0, Prev=None,
+             label='Title 1', destination=(0, 0, 1000)),
+        dict(Count=4, First=3, Last=6, Next=7, Parent=0, Prev=1,
+             label='Title 2', destination=(0, 0, 900)),
+        dict(Count=0, First=None, Last=None, Next=4, Parent=2, Prev=None,
+             label='Title 3', destination=(0, 50, 800)),
+        dict(Count=1, First=5, Last=5, Next=6, Parent=2, Prev=3,
+             label='Title 4', destination=(0, 0, 700)),
+        dict(Count=0, First=None, Last=None, Next=None, Parent=4, Prev=None,
+             label='Title 5', destination=(0, 0, 600)),
+        dict(Count=0, First=None, Last=None, Next=None, Parent=2, Prev=4,
+             label='Title 6', destination=(1, 0, 900)),
+        dict(Count=2, First=8, Last=8, Next=10, Parent=0, Prev=2,
+             label='Title 7', destination=(1, 0, 800)),
+        dict(Count=1, First=9, Last=9, Next=None, Parent=7, Prev=None,
+             label='Title 8', destination=(1, 0, 700)),
+        dict(Count=0, First=None, Last=None, Next=None, Parent=8, Prev=None,
+             label='Title 9', destination=(1, 0, 600)),
+        dict(Count=1, First=11, Last=11, Next=None, Parent=0, Prev=7,
+             label='Title 10', destination=(1, 0, 500)),
+        dict(Count=0, First=None, Last=None, Next=None, Parent=10, Prev=None,
+             label='Title 11', destination=(1, 0, 400))]
 
     root, bookmarks = get_bookmarks('''
         <h2>1</h2> level 1
@@ -97,7 +115,7 @@ def test_bookmarks():
         <h2>3</h2> level 1
         <h3>4</h3> level 2
         <h4>5</h4> level 3
-    ''')
+    ''', structure_only=True)
     assert root == dict(Count=5, First=1, Last=3)
     assert bookmarks == [
         dict(Count=1, First=2, Last=2, Next=3, Parent=0, Prev=None),
@@ -116,7 +134,7 @@ def test_bookmarks():
         <h2>7</h2> h2 level 2
         <h4>8</h4> h4 level 3
         <h1>9</h1> h1 level 1
-    ''')
+    ''', structure_only=True)
     assert root == dict(Count=9, First=1, Last=9)
     assert bookmarks == [
         dict(Count=3, First=2, Last=3, Next=5, Parent=0, Prev=None),
