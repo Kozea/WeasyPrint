4f4fc0ee8ece91cd2208f8b9e45d0835b8c427ee
Simon Sapin <simon.sapin@exyr.org>
2012-07-29 15:13:16 +0200

    Make some drawing tests more robust ... maybe?

--- weasyprint/tests/test_draw.py
+++ weasyprint/tests/test_draw.py
@@ -1447,7 +1447,7 @@ def test_before_after():
             <style>
                 @page { size: 300px 30px }
                 body { margin: 0; background: #fff }
-                a[href]:before { content: '[' '' attr(href) '] ' }
+                a[href]:before { content: '[' attr(href) '] ' }
             </style>
             <p><a href="some url">some content</p>
         '''),
@@ -1456,7 +1456,7 @@ def test_before_after():
                 @page { size: 300px 30px }
                 body { margin: 0; background: #fff }
             </style>
-            <p><a href="another url">[some url] some content</p>
+            <p><a href="another url"><span>[some url] </span>some content</p>
         ''')
     ], tolerance=10)
 
@@ -1474,8 +1474,11 @@ def test_before_after():
             <style>
                 @page { size: 500px 30px }
                 body { margin: 0; background: #fff }
+                q:before, q:after { content: none }
             </style>
-            <p>« Lorem ipsum “ dolor ” sit amet »</p>
+            <p><span><span>« </span>Lorem ipsum
+                <span><span>“ </span>dolor<span> ”</span></span>
+                sit amet<span> »</span></span></p>
         ''')
     ], tolerance=10)
 
@@ -1493,7 +1496,7 @@ def test_before_after():
                 @page { size: 100px 30px }
                 body { margin: 0; background: #fff }
             </style>
-            <p>a<img src="pattern.png" alt="Missing image">bc</p>
+            <p><span>a<img src="pattern.png" alt="Missing image">b</span>c</p>
         ''')
     ], tolerance=10)
 
