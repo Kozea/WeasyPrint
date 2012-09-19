30155388ad39b8ab8e51172762abb855526e9328
Simon Sapin <simon.sapin@exyr.org>
2012-09-19 15:00:38 +0200

    Prepare for merging the docs into master.
    Move documentation pages into /docs, remove everything else.

--- static/samples/gen.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-cd $(dirname $0)
-weasyprint http://www.webstandards.org/files/acid2/test.html acid2.pdf
-weasyprint http://www.webstandards.org/files/acid2/test.html acid2.png
-weasyprint http://www.w3.org/TR/CSS21/intro.html CSS21-intro.pdf -s CSS21-print.css
