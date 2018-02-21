8d868383e7c8f74d149479d303d52b40375a5b9c
Guillaume Ayoub <guillaume.ayoub@kozea.fr>
2018-02-21 03:01:42 +0100

    Remove Python2 compatibility

--- weasyprint/layout/flex.py
+++ weasyprint/layout/flex.py
@@ -1,4 +1,3 @@
-# coding: utf-8
 """
     weasyprint.layout.flex
     ------------------------
@@ -9,8 +8,6 @@
     :license: BSD, see LICENSE for details.
 
 """
-
-from __future__ import division, unicode_literals
 
 from math import log10
 
