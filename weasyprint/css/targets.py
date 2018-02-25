087cb3af91ae43f339d96a63fe4a31e16f5769bd
Tontyna <35614937+Tontyna@users.noreply.github.com>
2018-02-25 10:51:55 +0100

    cleanup code to pass Travis CI check

--- weasyprint/css/targets.py
+++ weasyprint/css/targets.py
@@ -140,7 +140,8 @@ class _TargetCollector(object):
         item = self.items.get(anchor_name, None)
         if item:
             LOGGER.debug(
-                'store_target? %s %s', anchor_name, TARGET_STATE.name(item.state))
+                'store_target? %s %s', anchor_name, 
+                TARGET_STATE.name(item.state))
             if item.state == TARGET_STATE.PENDING:
                 LOGGER.debug('   -> update: %s', target_counter_values)
                 # need A REAL DUPLICATE UNCONNECTED SEPARATE COPY!!
