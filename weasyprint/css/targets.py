6b43c834633b81dc4ec847d7424625713d8dc262
Guillaume Ayoub <guillaume.ayoub@kozea.fr>
2018-03-28 00:55:10 +0200

    Clean targets.py

--- weasyprint/css/targets.py
+++ weasyprint/css/targets.py
@@ -35,21 +35,12 @@ class TargetCollector(object):
     """Collector of HTML targets used by CSS content with ``target-*``."""
 
     def __init__(self):
-        self.reset()
-
-    def reset(self):
         self.had_pending_targets = False
         self.existing_anchors = []
         self.items = {}
 
-    def _addtarget(self, anchor_name):
-        return self.items.setdefault(anchor_name, TargetLookupItem())
-
     def collect_anchor(self, anchor_name):
-        """
-        stores `anchor_name` in `existing_anchors`
-        should be called by computed_values.anchor()
-        """
+        """Store ``anchor_name`` in ``existing_anchors``."""
         if anchor_name and isinstance(anchor_name, str):
             if anchor_name in self.existing_anchors:
                 LOGGER.warning('Anchor defined twice: %s', anchor_name)
@@ -57,56 +48,56 @@ class TargetCollector(object):
                 self.existing_anchors.append(anchor_name)
 
     def collect_computed_target(self, anchor_name):
-        """
-        stores a `computed` target's (internal!) anchor name,
-        verified by computed_values.content()
+        """Store a computed internal target's ``anchor_name``.
 
-        anchor_name without '#' and already unquoted
+        ``anchor_name`` must not start with '#' and be already unquoted.
+
         """
         if anchor_name and isinstance(anchor_name, str):
-            self._addtarget(anchor_name)
+            self.items.setdefault(anchor_name, TargetLookupItem())
 
     def lookup_target(self, anchor_name, source_box, parse_again_function):
-        """ called in content_to_boxes() when the source_box needs a target-*
-        returns a TargetLookupItem
-        if already filled by a previous anchor-element: up-to-date
-        else: pending, we must parse the whole thing again
+        """Get a TargetLookupItem corresponding to ``anchor_name``.
+
+        If it is already filled by a previous anchor-element, the status is
+        'up-to-date'. Otherwise, it is 'pending', we must parse the whole
+        tree again.
+
         """
-        item = self.items.get(
-            anchor_name,
-            TargetLookupItem('undefined'))
+        item = self.items.get(anchor_name, TargetLookupItem('undefined'))
+
         if item.state == 'pending':
-            if anchor_name not in self.existing_anchors:
-                item.state = 'undefined'
-            else:
+            if anchor_name in self.existing_anchors:
                 self.had_pending_targets = True
                 item.pending_boxes.setdefault(source_box, parse_again_function)
+            else:
+                item.state = 'undefined'
 
         if item.state == 'undefined':
             LOGGER.error(
-                'content discarded: target points to undefined anchor "%s"',
+                'Content discarded: target points to undefined anchor "%s"',
                 anchor_name)
-            # feedback to invoker: discard the parent_box
-            # at the moment it's `build.before_after_to_box()` which cares
-            source_box.style['content'] = 'none'
+
         return item
 
     def store_target(self, anchor_name, target_counter_values, target_box):
+        """Store a target called ``anchor_name``.
+
+        If there is a pending TargetLookupItem, it is updated. Only previously
+        collected anchors are stored.
+
         """
-        called by every anchor-element in build.element_to_box
-        if there is a pending TargetLookupItem, it is updated
-        only previously collected anchor_names are stored
-        """
-        item = self.items.get(anchor_name, None)
+        item = self.items.get(anchor_name)
         if item and item.state == 'pending':
             item.state = 'up-to-date'
             item.target_counter_values = copy.deepcopy(target_counter_values)
             item.target_box = target_box
 
     def check_pending_targets(self):
+        """Check pending targets if needed."""
         if not self.had_pending_targets:
             return
-        self.had_pending_targets = False
         for key, item in self.items.items():
             for _, function in item.pending_boxes.items():
                 function()
+        self.had_pending_targets = False
