1969c7155eea41fb5d32cc0ccdb7aa7585f8816e
Guillaume Ayoub <guillaume.ayoub@kozea.fr>
2018-08-22 14:37:55 +0200

    Add empty test file for flex

---
+++ weasyprint/tests/test_layout/test_flex.py
@@ -0,0 +1,10 @@
+"""
+    weasyprint.tests.layout.flex
+    ----------------------------
+
+    Tests for flex layout.
+
+    :copyright: Copyright 2018 Simon Sapin and contributors, see AUTHORS.
+    :license: BSD, see LICENSE for details.
+
+"""
