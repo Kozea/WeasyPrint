8624113e134d661cd58be2f8e77577cedc0acf64
Tontyna <35614937+Tontyna@users.noreply.github.com>
2018-09-01 11:20:38 +0200

    Improve fontconfig check Emit warnings about font-less fontconfig on any OS.
    Stick to native rendering on Windows and macOS if fontconfig has
    neither fonts nor a config file.

--- weasyprint/fonts.py
+++ weasyprint/fonts.py
@@ -21,8 +21,10 @@ from .urls import fetch
 
 # Cairo crashes with font-size: 0 when using Win32 API
 # See https://github.com/Kozea/WeasyPrint/pull/599
+# Probably it will crash on macOS, too, when native font rendering is used,
 # Set to True on startup when fontconfig is inoperable.
 # Used by text/Layout() to mask font-size: 0 with a font_size of 1.
+# TODO: Should we set it to true on Windows and macOS if Pango < 13800?
 ZERO_FONTSIZE_CRASHES_CAIRO = False
 
 
@@ -161,45 +163,79 @@ else:
     def _check_font_configuration(font_config, warn=False):
         """Check whether the given font_config has fonts.
 
-        The default fontconfig configuration file may be missing, particularly
-        on Windows, giving "Fontconfig error: Cannot load default config file".
+        The default fontconfig configuration file may be missing (particularly
+        on Windows or macOS, where installation of fontconfig isn't as
+        standardized as on Liniux), resulting in "Fontconfig error: Cannot load
+        default config file".
 
-        No default config == No fonts.
+        Fontconfig tries to retrieve the system fonts as fallback, which may or
+        may not work, especially on macOS, where fonts can be installed at
+        various loactions.
+        On Windows (at least since fontconfig 2.13) the fallback seems to work.
+
+        No default config && system fonts fallback fails == No fonts.
+        Config file exists, but doesn't provide fonts == No fonts.
         No fonts == expect ugly output.
-        If you happen to have an html without a valid @font-face all
-        letters turn into rectangles.
+
+        If you happen to have no fonts and an html without a valid @font-face
+        all letters turn into rectangles.
         If you happen to have an html with at least one valid @font-face
         all text is styled with that font.
 
+        On Windows and macOS we can cause Pango to use native font rendering
+        instead of rendering fonts with FreeType. But then we must do without
+        @font-face. Expect other missing features and ugly output.
         """
-        # Nobody ever complained about such a situation on Unix-like OSes.
-        if not sys.platform.startswith('win'):
+
+        # On Linux we can do nothing but give warnings.
+        has_native_mode = (
+            sys.platform.startswith('win') or
+            sys.platform.startswith('darwin'))
+        if not has_native_mode and not warn:
             return True
 
+        # Having fonts means: fontconfig's config file returns fonts or
+        # fontconfig managed to retrieve system fallback-fonts. On Windows the
+        # fallback stragegy seems to work since fontconfig >= 2.13
         fonts = fontconfig.FcConfigGetFonts(
             font_config, fontconfig.FcSetSystem)
+        # Of course, with nfont == 1 the user wont be happy, too...
         if fonts.nfont > 0:
             return True
 
-        if warn:
-            config_files = fontconfig.FcConfigGetConfigFiles(font_config)
-            config_file = fontconfig.FcStrListNext(config_files)
-            if config_file == ffi.NULL:
-                warnings.warn(
-                    '@font-face not supported: '
-                    'Cannot load default config file')
+        # whats the reason for zero fonts?
+        config_files = fontconfig.FcConfigGetConfigFiles(font_config)
+        config_file = fontconfig.FcStrListNext(config_files)
+        if config_file == ffi.NULL:
+            # no config file, no system fonts found
+            # on Windows and macOS it might help to fall back to native font
+            # rendering
+            if has_native_mode:
+                if warn:
+                    warnings.warn(
+                        '@font-face not supported: '
+                        'FontConfig cannot load default config file')
+                return False
             else:
-                warnings.warn('@font-face not supported: no fonts configured')
+                if warn:
+                    warnings.warn(
+                        'FontConfig cannot load default config file.'
+                        'Expect ugly output.')
+                return True
+        else:
+            # useless config file or indeed no fonts
+            if warn:
+                warnings.warn(
+                    'FontConfig: No fonts configured. '
+                    'Expect ugly output.')
+            return True
 
         # TODO: on Windows we could try to add the system fonts like that:
         # fontdir = os.path.join(os.environ['WINDIR'], 'Fonts')
         # fontconfig.FcConfigAppFontAddDir(
         #     font_config,
-        #     # not shure which encoding fontconfig expects
+        #     # not sure which encoding fontconfig expects
         #     fontdir.encode('mbcs'))
-
-        # Fall back to default @font-face-less behaviour.
-        return False
 
     class FontConfiguration(FontConfiguration):
         def __init__(self):
@@ -226,6 +262,7 @@ else:
                 fontconfig.FcConfigDestroy(self._fontconfig_config)
             else:
                 self.font_map = None
+
             # On Windows the font tempfiles cannot be deleted,
             # putting them in a subfolder made my life easier.
             self._tempdir = None
