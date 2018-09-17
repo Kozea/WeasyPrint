ff72a7972eace62ce228acfe6748c699e0ec5af7
Guillaume Ayoub <guillaume.ayoub@kozea.fr>
2018-09-17 18:48:35 +0200

    Fix comment

--- weasyprint/layout/float.py
+++ weasyprint/layout/float.py
@@ -28,7 +28,7 @@ def float_width(box, context, containing_block):
 def float_layout(context, box, containing_block, device_size, absolute_boxes,
                  fixed_boxes):
     """Set the width and position of floating ``box``."""
-    # avoid a circular imports
+    # Avoid circular imports
     from .blocks import block_container_layout
     from .flex import flex_layout
     from .inlines import inline_replaced_box_width_height
