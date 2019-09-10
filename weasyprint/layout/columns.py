48d8632eaa69ad56ed353715c70d07158c5ff7f0
grewn0uille <lucie.anglade@kozea.fr>
2019-09-10 12:09:42 +0200

    Avoid not in flow children to compute box height

--- weasyprint/layout/columns.py
+++ weasyprint/layout/columns.py
@@ -187,7 +187,8 @@ def columns_layout(context, box, max_position_y, skip_stack, containing_block,
                     next_box, _, _, _, _ = block_box_layout(
                         context, column_box, box.content_box_y(),
                         column_skip_stack, containing_block, True, [], [], [])
-                    next_box_size = next_box.children[0].margin_height()
+                    assert next_box.children[-1].is_in_normal_flow()
+                    next_box_size = next_box.children[-1].margin_height()
                 else:
                     empty_space = next_box_size = 0
 
