5fb675e3629d65af0c36b97aef82dbbe72e4e9e3
Guillaume Ayoub <guillaume@courtbouillon.org>
2021-03-31 15:20:57 +0200

    Fix svg tag size

--- weasyprint/svg/svg.py
+++ weasyprint/svg/svg.py
@@ -10,9 +10,6 @@ from .utils import preserve_ratio
 
 
 def svg(svg, node, font_size):
-    width, height = svg.get_intrinsic_size(font_size)
-    width = width or svg.concrete_width
-    height = height or svg.concrete_height
     scale_x, scale_y, translate_x, translate_y = preserve_ratio(
-        svg, node, font_size, width, height)
+        svg, node, font_size, svg.concrete_width, svg.concrete_height)
     svg.stream.transform(scale_x, 0, 0, scale_y, translate_x, translate_y)
