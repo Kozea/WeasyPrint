abd54c4bd46e36316d5eae44a6ca37547c8f726c
Lucie Anglade <lucie@courtbouillon.org>
2021-10-28 11:21:37 +0200

    Set SVG ratio when width and height are 0

--- tests/draw/svg/test_shapes.py
+++ tests/draw/svg/test_shapes.py
@@ -419,3 +419,26 @@ def test_rect_stroke_zero():
               stroke-width="0" stroke="red" fill="none" />
       </svg>
     ''')
+
+
+@assert_no_logs
+def test_rect_width_height_zero():
+    assert_pixels('rect_fill', 9, 9, '''
+        _________
+        _________
+        _________
+        _________
+        _________
+        _________
+        _________
+        _________
+        _________
+    ''', '''
+        <style>
+            @page { size: 9px }
+            svg { display: block }
+        </style>
+        <svg width="0" height="0" xmlns="http://www.w3.org/2000/svg">
+            <rect x="2" y="2" width="5" height="5" fill="red" />
+        </svg>
+    ''')
