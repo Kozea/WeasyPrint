f8c97479621a48e89e4a1e0b72f724cd3280da35
Lucie Anglade <lucie@courtbouillon.org>
2022-01-14 15:19:26 +0100

    Don’t crash when running elements aren’t ParentBoxes

--- tests/layout/test_page.py
+++ tests/layout/test_page.py
@@ -1342,3 +1342,21 @@ def test_running_elements_display():
         getattr(node, 'text', '') for node in center.descendants()) == 'block'
     assert ''.join(
         getattr(node, 'text', '') for node in right.descendants()) == 'table'
+
+
+@assert_no_logs
+def test_running_img():
+    # Test regression
+    render_pages('''
+      <style>
+        img {
+          position: running(img);
+        }
+        @page {
+          @bottom-center {
+            content: element(img);
+          }
+        }
+      </style>
+      <img src="pattern.png" />
+    ''')
