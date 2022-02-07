98ce588b1a8eb1b24f79876778e31e8cc68c334b
Guillaume Ayoub <guillaume@courtbouillon.org>
2022-02-07 15:59:25 +0100

    Use the same workaround everywhere for floating numbers

--- weasyprint/layout/block.py
+++ weasyprint/layout/block.py
@@ -473,8 +473,9 @@ def _in_flow_layout(context, box, index, child, new_children, page_is_empty,
             # https://www.w3.org/TR/css-break-3/#box-splitting
             # (Occasionally the order of this calculation would otherwise come
             # out with unequal float values, forcing the box to the next page.)
+            # The 1e-9 value comes from PEP 485.
             if (new_content_position_y >
-                    context.page_bottom - bottom_space + 0.001 and
+                    (context.page_bottom - bottom_space) * (1 + 1e-9) and
                     not page_is_empty_with_no_children):
                 # The child content overflows the page area, display it on the
                 # next page.
