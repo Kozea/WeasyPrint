2adfe066bd56fe1247f7cd537b6c377755a7e47b
Guillaume Ayoub <guillaume@courtbouillon.org>
2022-04-04 16:19:14 +0200

    Version 54.3

--- weasyprint/__init__.py
+++ weasyprint/__init__.py
@@ -16,7 +16,7 @@ import cssselect2
 import html5lib
 import tinycss2
 
-VERSION = __version__ = '54.2'
+VERSION = __version__ = '54.3'
 
 __all__ = [
     'HTML', 'CSS', 'Attachment', 'Document', 'Page', 'default_url_fetcher',
