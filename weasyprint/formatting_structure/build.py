e9c45c7060036a65b62dd05a944199be1f441ffd
Guillaume Ayoub <guillaume@courtbouillon.org>
2022-08-15 16:08:06 +0200

    Clean and test "text-transform: capitalize"
    Related to #1703.

--- weasyprint/formatting_structure/build.py
+++ weasyprint/formatting_structure/build.py
@@ -1252,7 +1252,7 @@ def process_text_transform(box):
             box.text = {
                 'uppercase': lambda text: text.upper(),
                 'lowercase': lambda text: text.lower(),
-                'capitalize': lambda text: capitalize(text),
+                'capitalize': capitalize,
                 'full-width': lambda text: text.translate(ASCII_TO_WIDE),
             }[text_transform](box.text)
         if box.style['hyphens'] == 'none':
@@ -1264,29 +1264,19 @@ def process_text_transform(box):
                 process_text_transform(child)
 
 
-def capitalize(text: str) -> str:
-    """Capitalize the first unicode "typographic_letter_unit" for each word whilst
-    leaving all other characters untouched.
-
-    This matches the CSS "text transform: capitalize" behaviour
-
-    """
+def capitalize(text):
+    """Capitalize words according to CSS’s "text-transform: capitalize"."""
     letter_found = False
-    output_text = list(text)
-
-    gc_text = [unicodedata.category(char) for char in text]
-
-    for i, cat in enumerate(gc_text):
-        if not letter_found:
-            if cat[0] in ("L", "N"):
-                letter_found = True
-                output_text[i] = output_text[i].upper()
-                pass
-
-        if cat[0] == "Z":
+    output = ''
+    for letter in text:
+        category = unicodedata.category(letter)[0]
+        if not letter_found and category in ('L', 'N'):
+            letter_found = True
+            letter = letter.upper()
+        elif category == 'Z':
             letter_found = False
-
-    return "".join(output_text)
+        output += letter
+    return output
 
 
 def inline_in_block(box):
