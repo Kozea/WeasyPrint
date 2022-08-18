9a4c5fc3c9816211cd671b5e803ccfae5b495b28
Guillaume Ayoub <guillaume@courtbouillon.org>
2022-08-18 15:22:45 +0200

    Fix the way column rules are drawn

--- tests/draw/test_column.py
+++ tests/draw/test_column.py
@@ -49,3 +49,34 @@ def test_column_rule_2(assert_pixels):
         <img src=blue.jpg>
         <img src=blue.jpg>
       </div>''')
+
+
+@assert_no_logs
+def test_column_rule_span(assert_pixels):
+    assert_pixels('''
+        ___________
+        ___________
+        ___________
+        ___a_______
+        ___a_r_a___
+        ___a_r_a___
+        ___________
+        ___________
+        ___________
+    ''', '''
+      <style>
+        img { display: inline-block; width: 1px; height: 1px }
+        div { columns: 2; column-rule: 1px solid red; column-gap: 3px }
+        article { column-span: all }
+        body { margin: 0; font-size: 0 }
+        @page { margin: 3px; size: 11px 9px }
+      </style>
+      <div>
+        <article>
+          <img src=blue.jpg>
+        </article>
+        <img src=blue.jpg>
+        <img src=blue.jpg>
+        <img src=blue.jpg>
+        <img src=blue.jpg>
+      </div>''')
