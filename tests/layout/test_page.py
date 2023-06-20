3c6696ed90edd20e7262e545d77ae2767df7156d
Michael Wedl <michael@syslifters.com>
2023-06-20 15:37:39 +0200

    Fix named pages inheritence

--- tests/layout/test_page.py
+++ tests/layout/test_page.py
@@ -818,6 +818,32 @@ def test_page_names_9():
 
 
 @assert_no_logs
+def test_page_names_10():
+    pages = render_pages('''
+      <style>
+        #running { position: running(running); }
+        #fixed { position: fixed; }
+        @page { size: 200px 200px; @top-center { content: element(header); }}
+        section { page: small; }
+        @page small { size: 100px 100px; }
+        .pagebreak { break-after: page; }
+      </style>
+      <div id="running">running</div>
+      <div id="fixed">fixed</div>
+      <section>
+        text
+        <div class="pagebreak"></div>
+        text
+      </section>
+    ''')
+    page1, page2 = pages
+
+    assert (page1.width, page1.height) == (100, 100)
+
+    assert (page2.width, page2.height) == (100, 100)
+
+
+@assert_no_logs
 @pytest.mark.parametrize('style, line_counts', (
     ('orphans: 2; widows: 2', [4, 3]),
     ('orphans: 5; widows: 2', [0, 7]),
