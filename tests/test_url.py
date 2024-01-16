178b507fba0bbbb81ba93b895cef2737414de282
Guillaume Ayoub <guillaume@courtbouillon.org>
2024-01-16 13:33:10 +0100

    Clean and add tests for malformed URLs

--- tests/test_url.py
+++ tests/test_url.py
@@ -1,24 +1,32 @@
-"""Test urls."""
+"""Test URLs."""
+
 import re
 
-from .testing_utils import FakeHTML, resource_path, capture_logs
+import pytest
+
+from .testing_utils import FakeHTML, capture_logs, resource_path
 
 
-def test_malformed_url_link():
-    """Test malformed URLs that contain a [ or ] character, which can be confused with a malformed IPv6 URL"""
+@pytest.mark.parametrize('url, base_url', (
+    ('https://weasyprint.org]', resource_path('<inline HTML>')),
+    ('https://weasyprint.org]', 'https://weasyprint.org]'),
+    ('https://weasyprint.org/', 'https://weasyprint.org]'),
+))
+def test_malformed_url_link(url, base_url):
+    """Test malformed URLs."""
     with capture_logs() as logs:
-        pdf = FakeHTML(string='''
-          <p><a href="https://weasyprint.org]">My Link</a></p>
-        ''', base_url=resource_path('<inline HTML>')).write_pdf()
+        pdf = FakeHTML(
+            string=f'<p><a href="{url}">My Link</a></p>',
+            base_url=base_url).write_pdf()
 
-        assert len(logs) == 1
-        assert "Malformed URL" in logs[0]
+    assert len(logs) == 1
+    assert "Malformed" in logs[0]
+    assert "]" in logs[0]
 
-        uris = re.findall(b'/URI \\((.*)\\)', pdf)
-        types = re.findall(b'/S (/\\w*)', pdf)
-        subtypes = re.findall(b'/Subtype (/\\w*)', pdf)
+    uris = re.findall(b'/URI \\((.*)\\)', pdf)
+    types = re.findall(b'/S (/\\w*)', pdf)
+    subtypes = re.findall(b'/Subtype (/\\w*)', pdf)
 
-        # 30pt wide (like the image), 20pt high (like line-height)
-        assert uris.pop(0) == b'https://weasyprint.org]'
-        assert subtypes.pop(0) == b'/Link'
-        assert types.pop(0) == b'/URI'
+    assert uris.pop(0) == url.encode()
+    assert subtypes.pop(0) == b'/Link'
+    assert types.pop(0) == b'/URI'
