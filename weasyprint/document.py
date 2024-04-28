b65d1ef58430e55b534b4bb59a55a0b0b24e33ff
Guillaume Ayoub <guillaume@courtbouillon.org>
2024-04-28 15:45:18 +0200

    Fix Attachment reference in documentation

--- weasyprint/document.py
+++ weasyprint/document.py
@@ -143,7 +143,7 @@ class DocumentMetadata:
         #: Extracted from the ``<meta name=dcterms.modified>`` element in HTML
         #: and written to the ``/ModDate`` info field in PDF.
         self.modified = modified
-        #: A list of :class:`attachments <Attachment>`, empty by default.
+        #: A list of :class:`attachments <weasyprint.Attachment>`, empty by default.
         #: Extracted from the ``<link rel=attachment>`` elements in HTML
         #: and written to the ``/EmbeddedFiles`` dictionary in PDF.
         self.attachments = attachments or []
