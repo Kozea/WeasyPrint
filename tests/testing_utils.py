9e8eaae25dfdfa0850499dcd19aa946054c1b338
David Huggins-Daines <dhd@ecolingui.ca>
2024-08-17 16:54:12 -0400

    test: only assert on non-debug logs

--- tests/testing_utils.py
+++ tests/testing_utils.py
@@ -1,6 +1,7 @@
 """Helpers for tests."""
 
 import functools
+import logging
 import sys
 from pathlib import Path
 
@@ -76,7 +77,7 @@ def assert_no_logs(function):
     """Decorator that asserts that nothing is logged in a function."""
     @functools.wraps(function)
     def wrapper(*args, **kwargs):
-        with capture_logs() as logs:
+        with capture_logs(level=logging.INFO) as logs:
             try:
                 function(*args, **kwargs)
             except Exception:  # pragma: no cover
