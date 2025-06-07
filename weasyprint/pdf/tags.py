fbae731a6863abba6b8dc756af10132a91756de2
Guillaume Ayoub <guillaume@courtbouillon.org>
2025-06-07 11:51:47 +0200

    Comment and document PDF tagging process

--- weasyprint/pdf/tags.py
+++ weasyprint/pdf/tags.py
@@ -10,6 +10,9 @@ from ..logger import LOGGER
 
 
 def add_tags(pdf, document, page_streams):
+    """Add tag tree to the document."""
+
+    # Add root structure.
     content_mapping = pydyf.Dictionary({})
     pdf.add_object(content_mapping)
     structure_root = pydyf.Dictionary({
@@ -27,6 +30,7 @@ def add_tags(pdf, document, page_streams):
     structure_root['K'] = pydyf.Array([structure_document.reference])
     pdf.catalog['StructTreeRoot'] = structure_root.reference
 
+    # Add tree content.
     content_mapping['Nums'] = pydyf.Array()
     links = []
     for page_number, (page, stream) in enumerate(zip(document.pages, page_streams)):
@@ -42,12 +46,14 @@ def add_tags(pdf, document, page_streams):
         nums = [reference for mcid, reference in sorted(nums.items())]
         content_mapping['Nums'][-1].extend(nums)
 
+    # Add annotations for links.
     for i, (link, annotation) in enumerate(links, start=len(document.pages)):
         content_mapping['Nums'].append(i)
         content_mapping['Nums'].append(link)
         annotation['StructParent'] = i
         annotation['F'] = 2 ** (2 - 1)
 
+    # Add extra metadata.
     pdf.catalog['ViewerPreferences'] = pydyf.Dictionary({'DisplayDocTitle': 'true'})
     pdf.catalog['MarkInfo'] = pydyf.Dictionary({'Marked': 'true'})
     if 'Lang' not in pdf.catalog:
@@ -55,9 +61,11 @@ def add_tags(pdf, document, page_streams):
         pdf.catalog['Lang'] = pydyf.String()
 
 
-def _get_marked_content_tag(box):
+def _get_pdf_tag(box):
+    """Get PDF tag corresponding to box."""
     if box.element is None:
         return 'NonStruct'
+
     tag = box.element_tag
     if tag == 'div':
         return 'Div'
@@ -100,11 +108,14 @@ def _get_marked_content_tag(box):
 
 
 def _build_box_tree(box, parent, pdf, page_number, nums, links, tags):
+    """Recursively build tag tree for given box."""
+
+    # Special case for absolute elements.
     if isinstance(box, AbsolutePlaceholder):
         box = box._box
 
     # Create box element.
-    tag = _get_marked_content_tag(box)
+    tag = _get_pdf_tag(box)
     if tag == 'LI':
         if parent['S'] == '/LI':
             # Anonymous list element, store as list item body.
@@ -133,6 +144,7 @@ def _build_box_tree(box, parent, pdf, page_number, nums, links, tags):
 
     # Handle special cases.
     if tag == 'Figure':
+        # Add extra data for images.
         x1, y1 = box.content_box_x(), box.content_box_y()
         x2, y2 = x1 + box.width, y1 + box.height
         element['A'] = pydyf.Dictionary({
@@ -145,11 +157,13 @@ def _build_box_tree(box, parent, pdf, page_number, nums, links, tags):
             source = box.element.attrib.get('src', 'unknown')
             LOGGER.error(f'Image "{source}" has no required alt description')
     elif tag == 'Table':
-        # TODO: handle tables correctly.
+        # Ignore table wrapper, map actual table.
         box, = box.children
     elif tag == 'TH':
+        # Set identifier for table headers to reference them in cells.
         element['ID'] = pydyf.String(id(box))
     elif tag == 'TD':
+        # Store table cell element to map it to headers later.
         # TODO: don’t use the box to store this.
         box.mark = element
 
@@ -164,40 +178,41 @@ def _build_box_tree(box, parent, pdf, page_number, nums, links, tags):
         pdf.add_object(object_reference)
         links.append((object_reference.reference, annotation))
 
-    def _add_children(children):
-        for child in children:
-            if isinstance(child, boxes.MarginBox):
-                # Build tree but don’t link it to main tree. It ensures that marked
-                # content is mapped in document and removed from list. It could be
-                # included in tree as Artifact, but that’s only allowed in PDF 2.0.
-                _build_box_tree(child, element, pdf, page_number, nums, links, tags)
-            elif isinstance(child, boxes.TextBox):
-                kid = tags.pop(child)
-                kid_element = pydyf.Dictionary({
-                    'Type': '/StructElem',
-                    'S': f'/{kid["tag"]}',
-                    'K': pydyf.Array([kid['mcid']]),
-                    'Pg': pdf.page_references[page_number],
-                    'P': element.reference,
-                })
-                pdf.add_object(kid_element)
-                element['K'].append(kid_element.reference)
-                assert kid['mcid'] not in nums
-                nums[kid['mcid']] = kid_element.reference
-            else:
-                if child.element_tag in ('ul', 'ol') and element['S'] == '/LI':
-                    child_parent = parent
-                else:
-                    child_parent = element
-                child_element = _build_box_tree(
-                    child, child_parent, pdf, page_number, nums, links, tags)
-                child_parent['K'].append(child_element.reference)
-
     if isinstance(box, boxes.ParentBox):
         # Build tree for box children.
         for child in box.children:
             children = child.children if isinstance(child, boxes.LineBox) else [child]
-            _add_children(children)
+            for child in children:
+                if isinstance(child, boxes.MarginBox):
+                    # Build tree but don’t link it to main tree. It ensures that marked
+                    # content is mapped in document and removed from list. It could be
+                    # included in tree as Artifact, but that’s only allowed in PDF 2.0.
+                    _build_box_tree(child, element, pdf, page_number, nums, links, tags)
+                elif isinstance(child, boxes.TextBox):
+                    # Add marked element from the stream.
+                    kid = tags.pop(child)
+                    kid_element = pydyf.Dictionary({
+                        'Type': '/StructElem',
+                        'S': f'/{kid["tag"]}',
+                        'K': pydyf.Array([kid['mcid']]),
+                        'Pg': pdf.page_references[page_number],
+                        'P': element.reference,
+                    })
+                    pdf.add_object(kid_element)
+                    element['K'].append(kid_element.reference)
+                    assert kid['mcid'] not in nums
+                    nums[kid['mcid']] = kid_element.reference
+                else:
+                    # Recursively build tree for child.
+                    if child.element_tag in ('ul', 'ol') and element['S'] == '/LI':
+                        # In PDFs, nested lists are linked to the parent list, but in
+                        # HTML, nested lists are linked to a parent’s list item.
+                        child_parent = parent
+                    else:
+                        child_parent = element
+                    child_element = _build_box_tree(
+                        child, child_parent, pdf, page_number, nums, links, tags)
+                    child_parent['K'].append(child_element.reference)
     else:
         # Add replaced box.
         assert isinstance(box, boxes.ReplacedBox)
@@ -206,6 +221,7 @@ def _build_box_tree(box, parent, pdf, page_number, nums, links, tags):
         assert kid['mcid'] not in nums
         nums[kid['mcid']] = element.reference
 
+    # Link table cells to related headers.
     if tag == 'Table':
         def _get_rows(table_box):
             for child in table_box.children:
@@ -214,32 +230,34 @@ def _build_box_tree(box, parent, pdf, page_number, nums, links, tags):
                 else:
                     yield from _get_rows(child)
 
-        # Get headers.
+        # Get headers and rows.
         column_headers = defaultdict(list)
-        line_headers = defaultdict(list)
+        row_headers = defaultdict(list)
         rows = tuple(_get_rows(box))
+
+        # Find column and row headers.
+        # TODO: handle rowspan and colspan values.
         for i, row in enumerate(rows):
-            # TODO: handle rowspan and colspan values.
             for j, cell in enumerate(row.children):
                 if cell.element is None:
                     continue
                 if cell.element_tag == 'th':
                     # TODO: handle rowgroup and colgroup values.
                     if cell.element.attrib.get('scope') == 'row':
-                        line_headers[i].append(pydyf.String(id(cell)))
+                        row_headers[i].append(pydyf.String(id(cell)))
                     else:
                         column_headers[j].append(pydyf.String(id(cell)))
+
+        # Map headers to cells.
         for i, row in enumerate(rows):
             for j, cell in enumerate(row.children):
                 if cell.element is None:
                     continue
                 if cell.element_tag == 'td':
-                    headers = []
-                    headers.extend(line_headers[i])
-                    headers.extend(column_headers[j])
+                    breakpoint()
                     cell.mark['A'] = pydyf.Dictionary({
                         'O': '/Table',
-                        'Headers': pydyf.Array(headers),
+                        'Headers': pydyf.Array(row_headers[i] + column_headers[j]),
                     })
 
     return element
