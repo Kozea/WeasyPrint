f44eae1c53c2b87e2133625928a02b882aa436d7
Gabriel Corona <corona.gabriel@gmail.com>
2025-06-10 00:55:52 +0200

    Add support for 'lh' and 'rlh' units

--- weasyprint/css/utils.py
+++ weasyprint/css/utils.py
@@ -40,7 +40,7 @@ RESOLUTION_TO_DPPX = {
 }
 
 # Sets of possible length units
-LENGTH_UNITS = set(LENGTHS_TO_PIXELS) | set(['ex', 'em', 'ch', 'rem'])
+LENGTH_UNITS = set(LENGTHS_TO_PIXELS) | set(['ex', 'em', 'ch', 'rem', 'lh', 'rlh'])
 
 # Constants about background positions
 ZERO_PERCENT = Dimension(0, '%')
