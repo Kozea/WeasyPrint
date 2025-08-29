aa794f907456fe483ebeeb77805c1d0dc7ceab9c
Guillaume Ayoub <guillaume@courtbouillon.org>
2025-08-29 17:23:28 +0200

    Add test and clean code for variables and functions in functions

--- weasyprint/css/__init__.py
+++ weasyprint/css/__init__.py
@@ -585,14 +585,9 @@ def resolve_var(computed, token, parent_style, known_variables=None):
         arguments = []
         for i, argument in enumerate(token.arguments):
             if argument.type == 'function':
-                # arguments.extend(resolve_var(
-                #     computed, argument, parent_style, known_variables))
-                # fix arguments.extend(resolve_var(
-                # TypeError: 'NoneType' object is not iterable
-                resolved_var = resolve_var(
+                resolved = resolve_var(
                     computed, argument, parent_style, known_variables)
-                if resolved_var:
-                    arguments.extend(resolved_var)
+                arguments.extend((argument,) if resolved is None else resolved)
             else:
                 arguments.append(argument)
         token = tinycss2.ast.FunctionBlock(
