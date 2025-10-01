0e86e9ac4a59ad1b1626cb25c9a62e44ba8952f6
Markus Mohanty <markus.mohanty@dedalus.com>
2025-10-01 10:22:28 +0200

    refactor duplication

--- weasyprint/pdf/metadata.py
+++ weasyprint/pdf/metadata.py
@@ -35,6 +35,12 @@ def add_metadata(pdf, metadata, variant, version, conformance, compress):
     pdf.catalog['Metadata'] = metadata.reference
 
 
+def description_element(rdf):
+    element = SubElement(rdf, f'{{{NS["rdf"]}}}Description')
+    element.attrib[f'{{{NS["rdf"]}}}about'] = ''
+    return element
+
+
 def generate_rdf_metadata(metadata, variant, version, conformance):
     """Generate RDF metadata as a bytestring.
 
@@ -44,58 +50,49 @@ def generate_rdf_metadata(metadata, variant, version, conformance):
     namespace = f'pdf{variant}id'
     rdf = Element(f'{{{NS["rdf"]}}}RDF')
 
-    element = SubElement(rdf, f'{{{NS["rdf"]}}}Description')
-    element.attrib[f'{{{NS["rdf"]}}}about'] = ''
+    element = description_element(rdf)
     element.attrib[f'{{{NS[namespace]}}}part'] = str(version)
     if conformance:
         element.attrib[f'{{{NS[namespace]}}}conformance'] = conformance
 
-    element = SubElement(rdf, f'{{{NS["rdf"]}}}Description')
-    element.attrib[f'{{{NS["rdf"]}}}about'] = ''
+    element = description_element(rdf)
     element.attrib[f'{{{NS["pdf"]}}}Producer'] = f'WeasyPrint {__version__}'
 
     if metadata.title:
-        element = SubElement(rdf, f'{{{NS["rdf"]}}}Description')
-        element.attrib[f'{{{NS["rdf"]}}}about'] = ''
+        element = description_element(rdf)
         element = SubElement(element, f'{{{NS["dc"]}}}title')
         element = SubElement(element, f'{{{NS["rdf"]}}}Alt')
         element = SubElement(element, f'{{{NS["rdf"]}}}li')
         element.attrib['xml:lang'] = 'x-default'
         element.text = metadata.title
     if metadata.authors:
-        element = SubElement(rdf, f'{{{NS["rdf"]}}}Description')
-        element.attrib[f'{{{NS["rdf"]}}}about'] = ''
+        element = description_element(rdf)
         element = SubElement(element, f'{{{NS["dc"]}}}creator')
         element = SubElement(element, f'{{{NS["rdf"]}}}Seq')
         for author in metadata.authors:
             author_element = SubElement(element, f'{{{NS["rdf"]}}}li')
             author_element.text = author
     if metadata.description:
-        element = SubElement(rdf, f'{{{NS["rdf"]}}}Description')
-        element.attrib[f'{{{NS["rdf"]}}}about'] = ''
+        element = description_element(rdf)
         element = SubElement(element, f'{{{NS["dc"]}}}subject')
         element = SubElement(element, f'{{{NS["rdf"]}}}Bag')
         element = SubElement(element, f'{{{NS["rdf"]}}}li')
         element.attrib['xml:lang'] = 'x-default'
         element.text = metadata.description
     if metadata.keywords:
-        element = SubElement(rdf, f'{{{NS["rdf"]}}}Description')
-        element.attrib[f'{{{NS["rdf"]}}}about'] = ''
+        element = description_element(rdf)
         element = SubElement(element, f'{{{NS["pdf"]}}}Keywords')
         element.text = ', '.join(metadata.keywords)
     if metadata.generator:
-        element = SubElement(rdf, f'{{{NS["rdf"]}}}Description')
-        element.attrib[f'{{{NS["rdf"]}}}about'] = ''
+        element = description_element(rdf)
         element = SubElement(element, f'{{{NS["xmp"]}}}CreatorTool')
         element.text = metadata.generator
     if metadata.created:
-        element = SubElement(rdf, f'{{{NS["rdf"]}}}Description')
-        element.attrib[f'{{{NS["rdf"]}}}about'] = ''
+        element = description_element(rdf)
         element = SubElement(element, f'{{{NS["xmp"]}}}CreateDate')
         element.text = metadata.created
     if metadata.modified:
-        element = SubElement(rdf, f'{{{NS["rdf"]}}}Description')
-        element.attrib[f'{{{NS["rdf"]}}}about'] = ''
+        element = description_element(rdf)
         element = SubElement(element, f'{{{NS["xmp"]}}}ModifyDate')
         element.text = metadata.modified
     return tostring(rdf, encoding='utf-8')
